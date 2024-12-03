fc4c526bbdd80f6fa9b8e5dc0c6447bd269f184b
Jeff Pursell <jpursell@gmail.com>
2024-12-03 18:25:51 -0500

    Finish 3a

--- src/bin/03a.rs
+++ src/bin/03a.rs
@@ -1,59 +1,36 @@
 use regex::Regex;
+#[derive(Debug)]
 struct Mul {
     a: usize,
     b: usize,
 }
 fn extract(str: &str) -> Vec<Mul> {
-    let re = Regex::new(r"mul\(([0-9]+),([0-9])+\)").unwrap();
+    let re = Regex::new(r"mul\(([0-9]+),([0-9]+)\)").unwrap();
     let mut out = Vec::new();
-    for (_, [a, b]) in re.captures_iter(str).map( |c|c.extract()) {
-        out.push(Mul{a, b});
+    for (_, [a, b]) in re.captures_iter(str).map(|c| c.extract()) {
+        let a = a.parse().unwrap();
+        let b = b.parse().unwrap();
+        out.push(Mul { a, b });
     }
     out
 }
 
-// fn process_line(a: &Vec<usize>, increasing: bool) -> bool {
-//     for chunk in a.windows(2) {
-//         let d = if increasing {
-//             chunk[1] as i64 - chunk[0] as i64
-//         } else {
-//             chunk[0] as i64 - chunk[1] as i64
-//         };
-//         let ok = match d {
-//             1 | 2 | 3 => true,
-//             _ => false,
-//         };
-//         if !ok {
-//             return false;
-//         }
-//     }
-//     true
-// }
-// fn process_line_at(a: &[usize], loc: usize) -> bool {
-//     let a: Vec<usize> = a
-//         .iter()
-//         .enumerate()
-//         .filter(|(i, _x)| i != &loc)
-//         .map(|(_i, x)| *x)
-//         .collect();
-//     process_line(&a, true) || process_line(&a, false)
-// }
-// fn process(vecs: Vec<Vec<usize>>) -> usize {
-//     let mut out = 0;
-//     for a in vecs.iter() {
-//         for i in 0..a.len() {
-//             let ok = process_line_at(a, i);
-//             if ok {
-//                 out += 1;
-//                 break;
-//             }
-//         }
-//     }
-//     out
-// }
+fn process(vecs: Vec<Mul>) -> usize {
+    let mut out = 0;
+    for a in vecs.iter() {
+        out += a.a * a.b;
+    }
+    out
+}
 fn main() {
-    let out = include_str!("03_test.txt");
+    // let out = include_str!("03_test.txt");
+    // let out = extract(out);
+    // let out = process(out);
+    // assert_eq!( out , 161);
+
+    let out = include_str!("03.txt");
     let out = extract(out);
     let out = process(out);
+    assert!(out > 1552138);
     println!("{out:?}");
 }
