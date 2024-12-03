712dd5d1ea34478f769279e7992c0ca9136ba293
Jeff Pursell <jpursell@gmail.com>
2024-12-03 08:33:58 -0500

    Finish 2a

--- src/bin/02a.rs
+++ src/bin/02a.rs
@@ -1,6 +1,4 @@
-
-
-fn extract(str:&str) -> Vec<Vec<usize>> {
+fn extract(str: &str) -> Vec<Vec<usize>> {
     let count = str.lines().count();
     let mut vecs = Vec::with_capacity(count);
     for line in str.lines() {
@@ -15,26 +13,35 @@ fn extract(str:&str) -> Vec<Vec<usize>> {
     vecs
 }
 
-fn process_line(a: &Vec<usize>) -> bool {
-    for (a, b) in a.chunks(2) {
-        match a.abs_diff(b) {
-            todo!()
+fn process_line(a: &Vec<usize>, increasing: bool) -> bool {
+    for chunk in a.windows(2) {
+        let d = if increasing {
+            chunk[1] as i64 - chunk[0] as i64
+        } else {
+            chunk[0] as i64 - chunk[1] as i64
+        };
+        let ok = match d {
+            1 | 2 | 3 => true,
+            _ => false,
+        };
+        if !ok {
+            return false;
         }
-
     }
+    true
 }
 fn process(vecs: Vec<Vec<usize>>) -> usize {
     let mut out = 0;
     for a in vecs.iter() {
-        for (a, b) in a.chunks(2) {
-
+        let ok = process_line(a, true) || process_line(a, false);
+        if ok {
+            out += 1;
         }
-        out += a * counter[a];
     }
     out
 }
 fn main() {
-    let out = include_str!("02_test.txt");
+    let out = include_str!("02.txt");
     let out = extract(out);
     let out = process(out);
     println!("{out:?}");
