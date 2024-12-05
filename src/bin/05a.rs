0fdab3a3c2e7f6104009eab7816fd722d1cb8354
Jeff Pursell <jpursell@gmail.com>
2024-12-05 07:08:24 -0500

    Work on 5a

--- src/bin/05a.rs
+++ src/bin/05a.rs
@@ -1,22 +1,95 @@
+use std::{collections::BTreeMap, str::FromStr};
 
-// fn extract(str: &str) -> Array2<Token> {
-//     let nrows = str.lines().count();
-//     let ncols = str.lines().next().unwrap().chars().count();
-//     let shape = (nrows, ncols);
-//     let mut tokens = Vec::with_capacity(nrows * ncols);
-//     for line in str.lines() {
-//         for char in line.chars() {
-//             match char {
-//                 'X' => tokens.push(Token::X),
-//                 'M' => tokens.push(Token::M),
-//                 'A' => tokens.push(Token::A),
-//                 'S' => tokens.push(Token::S),
-//                 _ => tokens.push(Token::O),
-//             }
-//         }
-//     }
-//     Array2::from_shape_vec(shape, tokens).unwrap()
-// }
+#[derive(Debug)]
+struct Rule {
+    left: usize,
+    right: usize,
+}
+
+impl FromStr for Rule {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let split = s.split_once("|");
+        if split.is_none() {
+            return Err(());
+        }
+        let (left, right) = split.unwrap();
+        let left = left.parse::<usize>();
+        if left.is_err() {
+            return Err(());
+        }
+        let left = left.unwrap();
+        let right = right.parse::<usize>();
+        if right.is_err() {
+            return Err(());
+        }
+        let right = right.unwrap();
+        Ok(Rule{left, right})
+    }
+}
+#[derive(Debug)]
+struct Update {
+    pages: Vec<usize>,
+}
+impl FromStr for Update {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let pages:Vec<&str> = s.split(",").collect();
+        if pages.is_empty() {
+            return Err(())
+        }
+        let pages = pages.iter().map(|p|p.parse::<usize>()).collect::<Vec<_>>();
+        if pages.iter().any(|p|p.is_err()) {
+            return Err(());
+        }
+        let pages: Vec<usize> = pages.iter().map(|p| *p.as_ref().unwrap()).collect();
+        Ok(Update{pages})
+    }
+}
+#[derive(Debug)]
+struct Puzzle {
+    rules: Vec<Rule>,
+    updates: Vec<Update>,
+}
+
+impl FromStr for Puzzle {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut get_rules = true;
+        let mut rules = Vec::new();
+        let mut updates = Vec::new();
+        for line in s.lines() {
+            if line.is_empty() {
+                get_rules = false;
+                continue;
+            }
+            if get_rules {
+                rules.push(line.parse::<Rule>()?);
+            } else {
+                updates.push(line.parse::<Update>()?);
+            }
+        }
+        Ok(Puzzle{rules,updates})
+    }
+}
+
+impl Puzzle {
+    fn process(&self) -> usize {
+        let mut ruleset = BTreeMap::new();
+        for Rule{left, right} in &self.rules {
+            if !ruleset.contains_key(&right) {
+                ruleset.insert(right, vec![left]);
+            } else {
+                ruleset.get_mut(&right).unwrap().push(left);
+            }
+        }
+        dbg!(&ruleset);
+        0
+    }
+}
 
 // fn process(tokens: ArrayView2<Token>) -> usize {
 //     let mut out = 0;
@@ -34,11 +107,10 @@
 // }
 
 fn main() {
-    let _out = include_str!("05_test.txt");
-    // let out = extract(out);
-    // dbg!(&out);
-    // let out = process(out.view());
-    // assert_eq!(out, 143);
+    let puzzle = include_str!("05_test.txt").parse::<Puzzle>().unwrap();
+    dbg!(&puzzle);
+    let out = puzzle.process();
+    assert_eq!(out, 143);
 
     // let out = include_str!("04.txt");
     // let out = extract(out);
