e6e52a480c0b0bf89514dd4eb3f5c87cdf05564c
Jeff Pursell <jpursell@gmail.com>
2024-12-16 08:43:12 -0500

    Work on 16 a

--- src/bin/16a.rs
+++ src/bin/16a.rs
@@ -7,7 +7,7 @@ enum Token {
     None,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 enum Direction {
     Up,
     Down,
@@ -69,36 +69,75 @@ impl FromStr for Puzzle {
     }
 }
 
-const DIRECTIONS: [Direction;4] = [Direction::Up, Direction::Down, Direction::Left, Direction::Right];
+const DIRECTIONS: [Direction; 4] = [
+    Direction::Up,
+    Direction::Down,
+    Direction::Left,
+    Direction::Right,
+];
 
 struct State {
-    positions_vec: Vec<[usize;2]>,
-    positions_set: BTreeSet<[usize;2]>,
+    positions_vec: Vec<[usize; 2]>,
+    positions_set: BTreeSet<[usize; 2]>,
     scores: Vec<usize>,
     directions: Vec<Direction>,
     print: bool,
     min_score: Option<usize>,
 }
+impl State {
+    fn add_move(&mut self, direction: Direction) {
+        let current_direction = self.directions.last().unwrap();
+        let next_position = direction
+            .position_from(self.positions_vec.last().unwrap())
+            .unwrap();
+        self.positions_set.insert(next_position);
+        self.positions_vec.push(next_position);
+        if current_direction == &direction {
+            self.scores.push(self.scores.last().unwrap() + 1);
+        } else {
+            self.scores.push(self.scores.last().unwrap() + 1001);
+        }
+        self.directions.push(direction);
+    }
+    fn remove_move(&mut self) {
+        let pos = self.positions_vec.pop().unwrap();
+        self.positions_set.remove(&pos);
+        self.scores.pop().unwrap();
+        self.directions.pop().unwrap();
+    }
+    fn remove_moves(&mut self, moves: usize) {
+        for _ in 0..moves {
+            self.remove_move();
+        }
+    }
+    fn update_min_score(&mut self) {
+        let score = self.scores.last().unwrap();
+        if let Some(min_score) = self.min_score {
+            self.min_score = Some(min_score.min(*score));
+        } else {
+            self.min_score = Some(*score);
+        }
+    }
+}
 impl Puzzle {
     fn process(&self, print: bool) -> usize {
-        let mut state = State{
-            positions_set: positions_set::BTreeSet::from([self.start]),
+        let mut state = State {
+            positions_set: BTreeSet::from([self.start]),
             positions_vec: vec![self.start],
             scores: vec![0],
             directions: vec![Direction::Right],
-            print
+            print,
             min_score: None,
         };
         self.find_lowest_score(&mut state);
         state.min_score.unwrap()
     }
-    fn find_lowest_score(
-        &self,
-        state: &mut State,
-    ) {
+    fn find_possible_dirs(&self, state: &State) -> Vec<Direction> {
+        let mut possible_directions = Vec::new();
         for next_direction in DIRECTIONS {
             {
-                let next_position = next_direction.position_from(state.positions_vec.last().unwrap());
+                let next_position =
+                    next_direction.position_from(state.positions_vec.last().unwrap());
                 if next_position.is_none() {
                     continue;
                 }
@@ -113,44 +152,48 @@ impl Puzzle {
                 if matches!(next_token.unwrap(), Token::Wall) {
                     continue;
                 }
-                state.positions_vec.push(next_position);
-                state.positions_set.insert(next_position);
             }
-            // we have a valid move at this point to we need to update our state
-            if &next_direction == state.directions.last().unwrap() {
-                state.scores.push(state.scores.last().unwrap() +1);
-            } else {
-                state.scores.push(state.scores.last().unwrap()+ 1001);
-            };
-            state.directions.push(next_direction);
-            todo!()
-            if positions_vec.last().unwrap() == &self.end {
-                if print {
-                    println!();
-                    println!("Score: {}", next_score);
-                    self.print_path(positions_set);
-                }
-                let pos = state.positions_vec.pop().unwrap();
-                state.positions_set.remove(&pos);
-                return Some(next_score);
-            }
-            if let Some(next_min_score) = self.find_lowest_score(
-                next_direction,
-                positions_set,
-                positions_vec,
-                next_score,
-                print,
-            ) {
-                if min_score.is_none() || next_min_score < min_score.unwrap() {
-                    min_score = Some(next_min_score);
-                }
-            }
-            let pos = positions_vec.pop().unwrap();
-            positions_set.remove(&pos);
+            possible_directions.push(next_direction);
         }
-        min_score
+        possible_directions
     }
-    fn print_path(&self, positions: &BTreeSet<[usize; 2]>) {
+    fn find_lowest_score(&self, state: &mut State) {
+        let mut moves = 0;
+        let mut directions = self.find_possible_dirs(state);
+        while directions.len() == 1 {
+            state.add_move(directions[0]);
+            moves += 1;
+            if state.positions_vec.last().unwrap() == &self.end {
+                if state.print {
+                    self.print_path(state);
+                }
+                state.update_min_score();
+                state.remove_moves(moves);
+                return;
+            }
+            directions = self.find_possible_dirs(state);
+        }
+        for next_direction in directions {
+            state.add_move(next_direction);
+            moves += 1;
+            if state.positions_vec.last().unwrap() == &self.end {
+                if state.print {
+                    self.print_path(state);
+                }
+                state.update_min_score();
+                state.remove_moves(moves);
+                return;
+            }
+            self.find_lowest_score(state);
+            state.remove_move();
+            moves -= 1;
+        }
+        state.remove_moves(moves);
+    }
+    fn print_path(&self, state: &State) {
+        let score = state.scores.last().unwrap();
+        println!();
+        println!("Score: {}", score);
         let mut current_row = 0;
         for (pos, token) in self.map.indexed_iter() {
             let pos = [pos.0, pos.1];
@@ -160,7 +203,7 @@ impl Puzzle {
             }
             print!(
                 "{}",
-                if positions.contains(&pos) {
+                if state.positions_set.contains(&pos) {
                     "O"
                 } else {
                     match token {
