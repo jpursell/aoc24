f4bbda84f16558376cf3eb9b27e5b3c7e2900a35
Jeff Pursell <jeffrey.pursell@maxar.com>
2024-12-18 16:24:23 -0500

    Finish 17 b

--- src/bin/17b.rs
+++ src/bin/17b.rs
@@ -50,49 +50,10 @@ struct Computer {
 }
 
 impl Computer {
-    // return true if output is consumed
-    fn run_instruction_reverse(&mut self, program: &[usize], output: usize) -> bool {
-        if self.instruction_pointer == 0 {
-            self.instruction_pointer = program.len() - 4;
-        } else {
-            self.instruction_pointer -= 2;
-        }
-        let instruction = Instruction::new(
-            program[self.instruction_pointer],
-            program[self.instruction_pointer + 1],
-        );
-        println!(
-            "DEBUG: ins: {}, reg: [{}, {}, {}], iptr: {}, out: {}",
-            instruction,
-            self.registers[0],
-            self.registers[1],
-            self.registers[2],
-            self.instruction_pointer,
-            output
-        );
-        match instruction {
-            Instruction::Adv(x) => self.adv_reverse(x),
-            Instruction::Bxl(x) => self.bxl_reverse(x),
-            Instruction::Bst(x) => self.bst_reverse(x),
-            Instruction::Jnz(_) => todo!(),
-            Instruction::Bxc(x) => self.bxc_reverse(x),
-            Instruction::Out(x) => self.out_reverse(x, output),
-            Instruction::Bdv(_) => todo!(),
-            Instruction::Cdv(x) => self.cdv_reverse(x),
-        }
-    }
     fn run_instruction(&mut self, program: &[usize]) -> Option<usize> {
         let instruction = Instruction::new(
             program[self.instruction_pointer],
             program[self.instruction_pointer + 1],
-        );
-        println!(
-            "DEBUG: ins: {}, reg: [{}, {}, {}], iptr: {}",
-            instruction,
-            self.registers[0],
-            self.registers[1],
-            self.registers[2],
-            self.instruction_pointer,
         );
         self.instruction_pointer += 2;
         match instruction {
@@ -113,57 +74,19 @@ impl Computer {
             _ => panic!(),
         }
     }
-    fn resolve_combo_reverse(&mut self, combo: usize, output: usize) {
-        match combo {
-            0..=3 => (),
-            4..=6 => {
-                // just set the lower 3 bits
-                self.registers[combo - 4] -= self.registers[combo - 4] % 8;
-                self.registers[combo - 4] += output;
-            }
-            _ => panic!(),
-        }
-    }
     fn adv(&mut self, combo: usize) -> Option<usize> {
         let combo: usize = self.resolve_combo(combo);
         self.registers[0] /= 2_usize.pow(combo as u32);
         None
     }
-    fn adv_reverse(&mut self, combo: usize) -> bool {
-        match combo {
-            0..=3 => {
-                self.registers[0] *= 2_usize.pow(combo as u32);
-            }
-            4..=6 => {
-                todo!();
-            }
-            _ => panic!(),
-        }
-        false
-    }
     fn bxl(&mut self, literal: usize) -> Option<usize> {
         self.registers[1] ^= literal;
         None
-    }
-    fn bxl_reverse(&mut self, literal: usize) -> bool {
-        self.registers[1] ^= literal;
-        false
     }
     fn bst(&mut self, combo: usize) -> Option<usize> {
         let combo: usize = self.resolve_combo(combo);
         self.registers[1] = combo % 8;
         None
-    }
-    fn bst_reverse(&mut self, combo: usize) -> bool {
-        match combo {
-            0..=3 => todo!(),
-            4..=6 => {
-                self.registers[4 - combo] -= self.registers[4 - combo] % 8;
-                self.registers[4 - combo] += self.registers[1] % 8;
-            }
-            _ => panic!(),
-        }
-        false
     }
     fn jnz(&mut self, literal: usize) -> Option<usize> {
         if self.registers[0] != 0 {
@@ -175,16 +98,8 @@ impl Computer {
         self.registers[1] ^= self.registers[2];
         None
     }
-    fn bxc_reverse(&mut self, _literal: usize) -> bool {
-        self.registers[1] ^= self.registers[2];
-        false
-    }
     fn out(&mut self, combo: usize) -> Option<usize> {
         Some(self.resolve_combo(combo) % 8)
-    }
-    fn out_reverse(&mut self, combo: usize, output: usize) -> bool {
-        self.resolve_combo_reverse(combo, output);
-        true
     }
     fn bdv(&mut self, combo: usize) -> Option<usize> {
         let combo: usize = self.resolve_combo(combo);
@@ -196,26 +111,6 @@ impl Computer {
         self.registers[2] = self.registers[0] / 2_usize.pow(combo as u32);
         None
     }
-    fn cdv_reverse(&mut self, combo: usize) -> bool {
-        // example of what happens
-        // reg[2] = reg[0] / 2.pow(reg[1])
-        //
-        // this is tough. I'm going to try updating reg[0] from reg[2]
-        // reg[0] = reg[2] * 2.pow(reg[1])
-        //
-        // that did not work, try updating reg[1]?
-        // reg[1] = log2(reg[0]/reg[2])
-        // no... in my case that would be a divide by zero
-        match combo {
-            0..=3 => todo!(),
-            4..=6 => {
-                self.registers[0] =
-                    self.registers[2] * 2_usize.pow(self.registers[combo - 4] as u32);
-            }
-            _ => panic!(),
-        }
-        false
-    }
     fn halted(&self, program: &[usize]) -> bool {
         self.instruction_pointer >= program.len()
     }
@@ -223,7 +118,6 @@ impl Computer {
 
 #[derive(Debug)]
 struct Puzzle {
-    registers: [usize; 3],
     program: Vec<usize>,
 }
 
@@ -232,15 +126,6 @@ impl FromStr for Puzzle {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let lines: Vec<&str> = s.lines().collect();
-        let read_register = |line: &str| {
-            let (_, line) = line.split_once(": ").unwrap();
-            line.parse::<usize>().unwrap()
-        };
-        let registers = [
-            read_register(lines[0]),
-            read_register(lines[1]),
-            read_register(lines[2]),
-        ];
         let program = lines[4];
         let (_, program) = program.split_once(": ").unwrap();
         let program = program
@@ -248,23 +133,47 @@ impl FromStr for Puzzle {
             .map(|s| s.parse::<usize>().unwrap())
             .collect();
 
-        Ok(Puzzle { registers, program })
+        Ok(Puzzle { program })
     }
 }
 
 impl Puzzle {
+    fn find_candidates(&self, base: usize, target: usize) -> Vec<usize> {
+        let mut out = Vec::new();
+        let program: &[usize] = &self.program;
+        for num in 0..8 {
+            let mut computer = Computer {
+                registers: [base + num, 0, 0],
+                instruction_pointer: 0,
+            };
+            let mut output = None;
+            while !computer.halted(program) {
+                if let Some(o) = computer.run_instruction(program) {
+                    output = Some(o);
+                    break;
+                }
+            }
+            if output.unwrap() == target {
+                out.push(num);
+            }
+        }
+        out
+    }
+    fn search(&self, base: usize, target_index: usize) -> Option<usize> {
+        let candidates = self.find_candidates(base, self.program[target_index]);
+        for candidate in candidates {
+            let new_base = (base + candidate) * 8;
+            if target_index == 0 {
+                return Some(base + candidate);
+            }
+            if let Some(output) = self.search(new_base, target_index - 1) {
+                return Some(output);
+            }
+        }
+        None
+    }
     fn process(&self) -> usize {
-        let mut computer = Computer {
-            registers: [0, 0, 0],
-            instruction_pointer: 0,
-        };
-        for desired_output in self.program.iter().rev() {
-            while !computer.run_instruction_reverse(&self.program, *desired_output) {}
-        }
-        while computer.instruction_pointer != 0 {
-            assert!(!computer.run_instruction_reverse(&self.program, 0));
-        }
-        computer.registers[0]
+        self.search(0, self.program.len() - 1).unwrap()
     }
 }
 
@@ -272,7 +181,7 @@ fn main() {
     let puzzle = include_str!("17.txt").parse::<Puzzle>().unwrap();
     let out = puzzle.process();
     println!("{out}");
-    // assert_eq!(out, )
+    assert_eq!(out, 266932601404433)
 }
 
 #[cfg(test)]
@@ -322,36 +231,20 @@ mod tests {
     }
 
     #[test]
-    fn test_bst_reverse() {
-        let start_registers = [15, 0, 0];
-        let mut computer = Computer {
-            registers: start_registers,
-            instruction_pointer: 0,
-        };
-        let program: &[usize] = &[2, 4];
-        // reg[1] = reg[0] % 8
-        computer.run_instruction(program);
-        // mess up the bits on purpose
-        computer.registers[0] = 9;
-        // should assign those 3 bits back
-        computer.run_instruction_reverse(program, 0);
-        assert_eq!(computer.registers[0], start_registers[0]);
-    }
-    
-    #[test]
-    fn test_bxl_reverse() {
-        let start_registers = [0, 5, 0];
-        let mut computer = Computer {
-            registers: start_registers,
-            instruction_pointer: 0,
-        };
-        let program: &[usize] = &[1, 3];
-        // reg[1] ^= 3
-        computer.run_instruction(program);
-        assert_eq!(computer.registers[1], 6);
-        assert_eq!(computer.instruction_pointer, 2);
-        computer.run_instruction_reverse(program, 0);
-        assert_eq!(computer.registers[1], start_registers[1]);
-        assert_eq!(computer.instruction_pointer, 0);
+    fn test_3_bit_inputs() {
+        let puzzle = include_str!("17.txt").parse::<Puzzle>().unwrap();
+        let expected = [7_usize, 6, 4, 7, 3, 2, 1, 0];
+        for (num, expected) in (0..8).zip(expected.iter()) {
+            let mut computer = Computer {
+                registers: [num, 0, 0],
+                instruction_pointer: 0,
+            };
+            let program: &[usize] = &puzzle.program;
+            while !computer.halted(program) {
+                if let Some(out) = computer.run_instruction(program) {
+                    assert_eq!(&out, expected);
+                }
+            }
+        }
     }
 }
